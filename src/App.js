add491bbe2e4ddd394d051f91f0b0e456f121b45
Robin Luongo <robin.luongo@gmail.com>
2019-08-05 19:23:16 -0400

    working scroll on nav

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import HeroContent from './components/HeroContent';
 import WhatWeDo from './components/WhatWeDo';
+import WhoWeAre from './components/WhoWeAre';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <HeroContent />
       <div className="hero content-border"></div>
       <WhatWeDo />
+      <WhoWeAre />
     </div>
   );
 }
--- src/components/WhoWeAre.js
+++ src/components/WhoWeAre.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import './WhatWeDo.css';
-import atlas from '../content/images/atlas.jpeg'
+import './WhoWeAre.css';
+import Atlas from '../content/images/atlas.jpeg'
 
 export default function Navbar() {
     return (
-        <div className="what-we-do content-border" id="whatWeDo">
+        <div className="who-we-are content-border">
             <div className="container two-column">
                 <div className="column-left">
                     <h2 className="content-title">Build the Right Program</h2>
                     <p className="content-text">User experience lean startup launch party. Infographic seed round innovator A/B testing iPad niche market. Stealth infographic advisor creative equity business-to-consumer. Hypotheses gen-z virality. Disruptive ecosystem MVP. Series A financing termsheet business-to-consumer hypotheses iteration sales low hanging fruit creative partnership accelerator freemium stealth startup. </p>
                 </div>
                 <div>
-                    <img src={atlas} />
+                    <img src={Atlas}/>
                 </div>
             </div>
         </div>
