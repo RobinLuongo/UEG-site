30b0a40a1411796127ab04fcf343d66f45143c00
unknown <robin.luongo@gmail.com>
2019-08-07 22:08:20 -0400

    styled contact section and began whoweare section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,14 +5,12 @@ export default function Contact() {
     return (
         <div className="contact content-border" id="partners">
             <div className="container">
-                <h2 className="content-title">From computers to coaches - we've got you covered</h2>
-                <p className="content-text">User experience lean startup launch party. Infographic seed round innovator A/B testing iPad niche market. Stealth infographic advisor creative equity business-to-consumer. Hypotheses gen-z virality. Disruptive ecosystem MVP. Series A financing termsheet business-to-consumer hypotheses iteration sales low hanging fruit creative partnership accelerator freemium stealth startup. </p>
+                <h2 className="content-title">How can we help?</h2>
+                <p className="content-text">Our dedicated team is ready to help. Please reach out with any questions or service inquiries.</p>
+                <h3></h3>
                 <div id="mc_embed_signup">
                     <form action="https://gmail.us3.list-manage.com/subscribe/post?u=9dcb1306505c56bb56d2794f6&amp;id=ca7fb407a4" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
                         <div id="mc_embed_signup_scroll">
-                            <h2>Subscribe</h2>
-                            <div class="indicates-required"><span class="asterisk">*</span>indicates required
-                            </div>
                             <div class="mc-field-group">
                                 <label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label>
                                 <input type="email" name="EMAIL" className="contact-input required email" id="mce-EMAIL" />
@@ -25,12 +23,16 @@ export default function Contact() {
                                 <label for="mce-LNAME">Last Name </label>
                                 <input type="text" name="LNAME" className="contact-input" id="mce-LNAME" />
                             </div>
+                            <div class="mc-field-group">
+	                            <label for="mce-MMERGE5">Message </label>
+                                <textarea form="mc-embedded-subscribe-form" rows="5" cols="33" name="MMERGE5" className="contact-input" id="mce-MMERGE5"/>
+                            </div>
                             <div id="mce-responses" class="clear">
                                 <div class="response" id="mce-error-response" style={{display:"none"}}></div>
                                 <div class="response" id="mce-success-response" style={{display:"none"}}></div>
                             </div>
-                            <div style={{position: "absolute", left: -5000}} aria-hidden="true"><input type="text" name="b_9dcb1306505c56bb56d2794f6_ca7fb407a4" tabindex="-1" value="" /></div>
-                            <div class="clear"><input type="submit" value="Submit" name="subscribe" id="mc-embedded-subscribe" class="button" /></div>
+                            <div style={{position: "absolute", left: -5000}} aria-hidden="true"><input type="text" name="b_9dcb1306505c56bb56d2794f6_ca7fb407a4" tabindex="-1" /></div>
+                            <div class="clear"><input type="submit" value="Submit" name="subscribe" id="mc-embedded-subscribe" className="submit-btn" /></div>
                         </div>
                     </form>
                 </div>
--- src/components/WhoWeAre.js
+++ src/components/WhoWeAre.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import './WhoWeAre.css';
-import Atlas from '../content/images/headshots/atlas.jpeg'
+import atlas from '../content/images/headshots/atlas.jpeg';
+import jesse from '../content/images/headshots/jesse.jpeg';
+import alex from '../content/images/headshots/alex.jpeg';
+import calvin from '../content/images/headshots/calvin.jpeg';
+import kyawt from '../content/images/headshots/kyawt.jpg';
+import suresh from '../content/images/headshots/suresh.jpg';
+import matthew from '../content/images/headshots/matthew.jpg';
+import robin from '../content/images/headshots/robin.jpg';
 
 export default function WhoWeAre() {
     return (
         <div className="who-we-are content-border">
-            <div className="container two-column">
-                <div className="column-left">
-                    <h2 className="content-title">Meet our team</h2>
-                    <p className="content-text">User experience lean startup launch party. Infographic seed round innovator A/B testing iPad niche market. Stealth infographic advisor creative equity business-to-consumer. Hypotheses gen-z virality. Disruptive ecosystem MVP. Series A financing termsheet business-to-consumer hypotheses iteration sales low hanging fruit creative partnership accelerator freemium stealth startup. </p>
+            <h2 className="content-title">Meet our Team</h2>
+            <div className="two-column">
+                <div className="bio-container left-column">
+                    <div className="bio-content">
+                        <div className="bio-name">Matthew Bronson, Advisor</div>
+                        <p className="bio-text">A veteran educator and higher ed consultant, Matthew has over 25 years of experience in higher education assessment and executive planning. He has led reaccreditation efforts at numerous institutions and guided countless programs through the planning and implementation phases of innovative co-curricular initiatives. He is currently advisor to the UC Davis-based LibreText, the largest open educational resource (OER) initiative in the world. Matthew brings to Uni his deep experience with program design and a passion for charting the edge of innovation in higher education.</p>
+                    </div>
+                    <img className="bio-image" src={matthew}/>
                 </div>
-                <div>
-                    <img src={Atlas}/>
+                <div className="bio-container right-column">
+                    <div className="bio-content">
+                        <div className="bio-name">Suresh Apavoo, Advisor</div>
+                        <p className="bio-text">Suresh Appavoo has over 18 years in diversity leadership roles in higher education. Before assuming his role the Chief Diversity and Inclusivity Officer for Southern Oregon University, Suresh served as both the Dean for Equity and Diversity and the Director for the Center of Diversity at the Dominican University of California. Suresh joins Uni with the goal of helping steer esports toward a more equitable and diverse future on college and university campuses across the United States.</p>
+                    </div>
+                    <img className="bio-image" src={suresh}/>
+                </div>
+            </div>
+            <div className="two-column">
+                <div className="bio-container left-column">
+                    <div className="bio-content">
+                        <div className="bio-name">Alex McNeil, CEO</div>
+                        <p className="bio-text">Alex has bootstrapped two successful businesses in the education space, overseeing them from startup to multiple employee exits. With North Coast Prep, Alex became a thought leader in the North Bay Area about the negative effects of academic overwork on student mental wellbeing. He holds an MA from San Francisco State in political theory, and cares deeply about issues of equity and mental health in education. He leads Uni as CEO, coordinating efforts to build the first generation of best-in-class collegiate esports programs. In his spare time he writes op-eds and reads science fiction.</p>
+                    </div>
+                    <img className="bio-image" src={alex}/>
+                </div>
+                <div className="bio-container right-column">
+                    <div className="bio-content">
+                        <div className="bio-name">Jesse Bodony, COO</div>
+                        <p className="bio-text">Jesse treads the line between the worlds of traditional athletics and esports. Having captained the U23 division of the Colorado Rapids MLS team and consistently reached grandmaster in both Starcraft II and Hearthstone, Jesse understands the demands that face student-athletes and student-gamers throughout school. Jesse holds an MA in Humanities from Dominican University, where his scholarship was recognized for its rigor and creativity. Jesse brings his expertise and strong leadership experience to Uni, where he leads the team in creating the first generation of best-in-class collegiate esports programs. </p>
+                    </div>
+                    <img className="bio-image" src={jesse}/>
                 </div>
             </div>
         </div>
