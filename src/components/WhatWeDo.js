ac22010ebbce06dc819a23e398cf5a3c6f713c68
unknown <robin.luongo@gmail.com>
2019-08-11 19:44:12 -0400

    some spacing fixes and some responsive design

--- src/components/WhatWeDo.js
+++ src/components/WhatWeDo.js
@@ -8,10 +8,10 @@ export default function WhatWeDo() {
             <div className="container two-column">
                 <div className="column-left">
                     <h2 className="content-title">Build the Right Program</h2>
-                    <p className="content-text">We work with you to build a best-in-class esports program tailored to the unique culture, strategic goals, and values of your institution. We create a custom profile and readiness report of your school, assessing the motivations, needs, and challenges that your institution will face in creating its esports program. From there, we help build the concrete capacities--such as facilities, coaching, recruitment, and more--necessary for a successful esports launch.
+                    <p className="content-text">We work with you to build a best-in-class esports program tailored to the unique culture, strategic goals, and values of your institution. We create a custom profile and readiness report of your school, assessing the motivations, needs, and challenges that your institution will face in creating its esports program. From there, we help build the concrete capacities—such as facilities, coaching, recruitment, and more—necessary for a successful esports launch.
                     </p>
                     <p className="content-text">
-                    We pride ourselves on building esports programs that represent the best your institution has to offer - inclusivity, competitive excellence, and a commitment to supporting your students’ passion and expression.
+                    We pride ourselves on building esports programs that represent the best your institution has to offer: inclusivity, competitive excellence, and a commitment to supporting your students’ passion and expression.
                     </p>
                 </div>
                 <div>
--- src/components/WhoWeAre.js
+++ src/components/WhoWeAre.js
@@ -12,7 +12,7 @@ import robin from '../content/images/headshots/robin.jpg';
 export default function WhoWeAre() {
     return (
         <div className="who-we-are content-border">
-            <h2 className="content-title">Meet our Team</h2>
+            <h2 className="content-title">Meet Our Team</h2>
             <div className="container two-column">
                 <div className="bio-container left-column">
                     <div className="bio-content">
@@ -45,13 +45,27 @@ export default function WhoWeAre() {
                 <div className="bio-container left-column">
                     <div className="bio-content">
                         <div className="bio-name">Kyawt Thiri Nyunt, CDO</div>
-                        <p className="bio-text">Kyawt is an internationally-recognized short-film artist who holds an MBA from Dominican University’s school of business. Her work (in artistic, academics, and business) has focused on issues of representation, belonging, and  diversity in her native country of Myanmar. She is an avid gamer who leads Uni’s diversity and inclusion efforts as Chief Diversity Officer, where she helps build programs that address the issues of discrimination and exclusion endemic to esports from their inception. </p>
+                        <p className="bio-text">Kyawt is an internationally-recognized short-film artist who holds an MBA from Dominican University’s school of business. Her work has focused on issues of representation, belonging, and  diversity in her native country of Myanmar. She is an avid gamer who leads Uni’s diversity and inclusion efforts as Chief Diversity Officer, where she helps build programs that address the issues of discrimination and exclusion endemic to esports. </p>
                     </div>
                 </div>
                 <div className="bio-container right-column">
                     <div className="bio-content">
                         <div className="bio-name">Robin Luongo, CTO</div>
-                        <p className="bio-text">Robin learned about programming at the same time he started gaming, at age six, when his dad worked as a developer for the iconic software company Broderbund. As a former software engineer and network and systems administrator, Robin fills the role of CTO at Uni, helping schools to understand the all-important technological side of esports and program development. Robin is a classically-trained cellist who plays in a string quartet during his spare time. </p>
+                        <p className="bio-text">Robin learned about programming at the same time he started gaming, at age six, when his dad worked as a developer for the iconic software company Broderbund. As a former software engineer and network and systems administrator, Robin fills the role of CTO at Uni, helping schools to understand the all-important technological side of esports and program development.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="container two-column">
+                <div className="bio-container left-column">
+                    <div className="bio-content">
+                        <div className="bio-name">Calvin Bryne, Legal Counsel</div>
+                        <p className="bio-text">Calvin holds a JD from the University of California Irvine. A staunch advocate for human rights, Calvin has worked for the ACLU, the UCI Human Rights Clinic, and several other non-profit organizations. Calvin has gamed since he was young and brings his passion for esports to his role as legal counsel for Uni. </p>
+                    </div>
+                </div>
+                <div className="bio-container right-column">
+                    <div className="bio-content">
+                        <div className="bio-name">Atlas Tanguay, Director of Operations</div>
+                        <p className="bio-text">With a background in behavioral healthcare management, Atlas brings experience facilitating communication between private and institutional stakeholders. At Uni, Atlas manages client workflows to ensure programs are developed on time, to standard, and aligned with your vision. A life-long gamer, he is passionate about manifesting the future of collegiate esports alongside our member institutions. </p>
                     </div>
                 </div>
             </div>
