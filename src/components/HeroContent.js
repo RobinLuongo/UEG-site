be1ca68a4d6081fc2f304724a0208c53e7e04b57
unknown <robin.luongo@gmail.com>
2019-08-18 17:51:07 -0400

    put up whitepaper and moved button

--- src/components/HeroContent.js
+++ src/components/HeroContent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './HeroContent.css';
 import download from '../content/images/download-white.png';
+import whitepaper from '../content/docs/UEG_White_Paper.pdf'
 
 export default function HeroContent(props) {
     return (
@@ -9,7 +10,7 @@ export default function HeroContent(props) {
                 <h1 className="hero-content-title">State of the Game: Collegiate Esports and the Future of University Gaming</h1>
                 <h2 className="hero-content-teaser">In this White Paper we break down the current state of collegiate esports, covering program development and administrative sentiment while highlighting the greatest benefits and obstacles to starting a program. With data collected from our national survey of over 400 university Athletic Directors, our report provides the most conprehensive available snapshot of the collegiate esports space.</h2>
                 <span className="download-link">
-                    <a src="#" onClick={(e) => props.openModal(e)}>
+                    <a href={whitepaper} download>
                         <img src={download} />
                         Download the Report
                     </a>
