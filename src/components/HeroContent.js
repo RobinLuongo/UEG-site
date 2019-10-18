a85d174bdb8202a514c277637dfd654145f61ab0
unknown <robin.luongo@gmail.com>
2019-10-18 00:16:25 -0700

    changed copy on homgepage

--- src/components/HeroContent.js
+++ src/components/HeroContent.js
@@ -1,19 +1,21 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
 import './HeroContent.css';
-import download from '../content/images/download-white.png';
+import arrow from '../content/images/arrow-right.png';
 import whitepaper from '../content/docs/UEG_White_Paper.pdf'
 
 export default function HeroContent(props) {
     return (
         <div className="hero-content">
             <div className="container">
-                <h1 className="hero-content-title">State of the Game: Collegiate Esports and the Future of University Gaming</h1>
-                <h2 className="hero-content-teaser">In this White Paper we break down the current state of collegiate esports, covering program development and administrative sentiment while highlighting the greatest benefits and obstacles to starting a program. With data collected from our national survey of over 400 university Athletic Directors, our report provides the most conprehensive available snapshot of the collegiate esports space.</h2>
+                <h1 className="hero-content-title">How much would building an esports program really cost?</h1>
+                <h2 className="hero-content-teaser">Discover the costs and benefits of building an esports program at your school with our custom pricing tool. With just a few points of data, we estimate the costs of building an esports program at your school--and provide information about potential benefits for recruitment and retention.</h2>
                 <span className="download-link">
-                    <a href={whitepaper} download>
-                        <img src={download} alt="download collegiate esports whitepaper"/>
-                        Download the Report
-                    </a>
+                    <Link href={whitepaper} to="/price-tool">
+                        <img src={arrow} alt="download collegiate esports whitepaper"/>
+                        Esports Price Tool
+                    </Link>
                 </span>
             </div>
         </div>
